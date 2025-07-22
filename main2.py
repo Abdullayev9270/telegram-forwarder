cfa92666735a8c20127d10003226b5954a8a8a3c
Abdullayev9270 <abdullayev7092@gmail.com>
2025-07-22 15:56:05 +0500

    main2.py

--- main2.py
+++ main2.py
@@ -1,43 +1,29 @@
-from telethon import TelegramClient, events
 
-# 👉 API ma'lumotlaringiz
+#-------------------------------------------------------------
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+
 api_id = 29742884
 api_hash = 'd3f0e1677c841276399e9b2b24151463'
 
-# 👉 Ruxsat etilgan foydalanuvchi ID'lari (bir nechta bo'lishi mumkin)
-allowed_user_ids = [
-    223606463,  # misol uchun foydalanuvchi 1
-    632060122,  # misol uchun foydalanuvchi 2
-]
+# Bu yerga 1-qadamda olingan stringni joylashtiring:
+string_session = "1ApWapzMBu6taZKDoO7Btvj5rGNL5AJ2NM5NqBIIGWUEU_ND3nWi61T2Tn5-EcBMZZ2EiIwC1O4cXGrikqp0_4QCsHUWd_DgTQdxaSGL-NRMKxSTZEyB7t8DYdLlW0Y9onJ06e2_DWiNATR3-3KIpmNdcfo55ek1oROqIVAOrOjdSDy_Bt4drlzVqn5ODaJdvsqkrDZJGnFoi5XLreRPQw0VfdIEMAK7y0HK11Hge46M3agCbhpRUqrKlQMjPWc1FMTSeLsanoz6QwlrbHjGRnPqC1WwVYRV6_kRJpBz3iFXxokUJyBJccczwPGI7N6ms8ss9c8bQpP9R7xV48qv3S7XKslqSRug="
 
-# 👉 Fayllar forward qilinadigan kanal username yoki ID
-channel = '@myphddoc'  # yoki -100XXXXXXXXXXXX
+client = TelegramClient(StringSession(string_session), api_id, api_hash)
 
-# 👉 Client sessiyasini ishga tushiramiz
-client = TelegramClient('my_session', api_id, api_hash)
+allowed_user_ids = [223606463,  632060122,  ]
+channel_username = '@myphddoc'
 
-# 👉 Har bir yangi xabarni kuzatamiz
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
     sender = await event.get_sender()
-
-    # Faqat ruxsat etilgan foydalanuvchilardan bo‘lsa
     if sender.id not in allowed_user_ids:
-        print(f"❌ Ruxsat yo‘q: {sender.id}")
         return
-
-    # Hujjat bo‘lsa va .doc/.docx/.pdf formatda bo‘lsa
     if event.document:
         file_name = event.document.attributes[0].file_name.lower()
         if file_name.endswith(('.doc', '.docx', '.pdf')):
-            print(f"📄 Qabul qilindi: {file_name} ({sender.id} dan)")
-            await client.send_file(channel, event.document, caption=f"Yangi hujjat: {file_name}")
-        else:
-            print(f"⛔ Noto‘g‘ri fayl turi: {file_name}")
-    else:
-        print("⚠️ Hujjat emas.")
+            await client.send_file(channel_username, event.document, caption=f"Yangi hujjat: {file_name}")
 
-# 👉 Clientni ishga tushuramiz
 client.start()
-print("🚀 Dastur ishga tushdi. Lichkaga kelgan fayllar kuzatilmoqda...")
 client.run_until_disconnected()
+
